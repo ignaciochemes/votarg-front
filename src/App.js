aca677c8d1f2bbae63fd80f31c2df4b45aab4896
ignacio gonzalez <ignacio.gonzalez@virtualdreams.io>
2022-05-30 18:01:10 -0300

    Agrego health check

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import MainGrid from "./Components/MainGrid";
 import NavbarComponent from "./Components/NavbarComponent";
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './Utils/Theme';
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./Utils/Theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HealthCheckComponent from "./Components/HealthCheckComponent";
 
 function App() {
     return (
-        <div style={{
-            backgroundColor: "#e7e7e7",
-            height: "100wh",
-        }}>
+        <div
+            style={{
+                backgroundColor: "#e7e7e7",
+                height: "100wh",
+            }}
+        >
             <ThemeProvider theme={theme}>
-                <NavbarComponent />
-                <MainGrid />
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={[<NavbarComponent />, <MainGrid />]}
+                        />
+                        <Route
+                            path="/health-check"
+                            element={<HealthCheckComponent />}
+                        />
+                    </Routes>
+                </BrowserRouter>
             </ThemeProvider>
         </div>
     );
---
+++ src/Components/HealthCheckComponent.js
@@ -0,0 +1,8 @@
+export default function HealthCheckComponent() {
+    return (
+        <div>
+            <h1>Health Check</h1>
+            <p>Nada para mostrar en esta pagina.</p>
+        </div>
+    );
+}
